4897d4161f50d0530bd084cebfb40392afc14910
Nikita Mathur <nikitamathur2015@gmail.com>
2015-04-04 03:06:22 +0530

    Deleted old login code

--- newStreet/login/urls.py
+++
@@ -1,6 +0,0 @@
-from django.conf.urls import patterns,url
-from django.contrib import admin
-from login import views
-urlpatterns = patterns('',
-    url(r'^register/$',views.register,name='register')
-)
