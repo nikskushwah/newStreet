cad8f463386e3cee8a5c90f1b618e0b80d81b0e4
Nikita Mathur <nikitamathur2015@gmail.com>
2015-05-01 02:59:51 +0530

    added for further changes

---
+++ newstreet-temp2/MINOR/rango/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth.models import User
+from rango.models import Category, Page, UserProfile,newws
+from django.forms import ModelForm, Textarea
+
+from django.core.exceptions import ValidationError
+
+from django_password_strength.widgets import PasswordStrengthInput, PasswordConfirmationInput
+
+
+class UserForm(forms.ModelForm):
+    username = forms.CharField(help_text="Please enter a username.")
+    email = forms.CharField(help_text="Please enter your email.")
+    password = forms.CharField(widget=forms.PasswordInput(), help_text="Please enter a password.", min_length = 6)
+    """passphrase = forms.CharField(widget=PasswordStrengthInput(), help_text = "Please enter a password.")
+    confirm_passphrase = forms.CharField(widget=PasswordConfirmationInput(), help_text = "Please enter a password.")"""
+    """if len(password) < 6:
+            raise forms.ValidationError("The new password must be at least 6 characters long." )"""
+    
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password',)
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('website', 'picture')
+
+
+class newsform(forms.ModelForm):
+    news=forms.CharField(widget=forms.Textarea)
+    longitude=forms.DecimalField()
+    latitude=forms.DecimalField()
+    class Meta:
+        model = newws
+        fields = ('news','longitude','latitude',)
